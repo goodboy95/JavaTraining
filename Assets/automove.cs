179c5a8651b754dc6aec0e5f0c25c56b5c8a76f0
goodboy95 <501217662@qq.com>
2018-06-25 00:13:20 +0800

    init

---
+++ Assets/automove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.name == "Car_6")
+            this.transform.Translate(0, 0, 0.7f);
+        else if (name == "Car_5")
+            transform.Translate(0, 0, 0.69f);
+	}
+}
